8cfa178e5350534596be662aac4238f4207062ad
Johurul Haque <johurulhaquejony@gmail.com>
2024-04-23 02:05:21 +0600

    fixed minor bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
       <FormField setTableData={setTableData} />
 
-      {tableData.length && (
+      {!!tableData.length && (
         <Table data={tableData} setTableData={setTableData} />
       )}
     </>

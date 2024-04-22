2233870216729598cc70bbb6c4f7d084cda86e28
Johurul Haque <johurulhaquejony@gmail.com>
2024-04-22 15:42:49 +0600

    removed res param

--- src/lib/catch-async.ts
+++ src/lib/catch-async.ts
@@ -2,16 +2,13 @@ import { dbConnect } from '@/config/db';
 import { NextRequest, NextResponse } from 'next/server';
 import { handleErrors } from './handle-errors';
 
-type HandlerFunc<T> = (
-  req: NextRequest,
-  res: NextResponse
-) => Promise<NextResponse<T>>;
+type HandlerFunc<T> = (req: NextRequest) => Promise<NextResponse<T>>;
 
 export function catchAsync<T>(fn: HandlerFunc<T>) {
-  return async (req: NextRequest, res: NextResponse) => {
+  return async (req: NextRequest) => {
     try {
       await dbConnect();
-      return await fn(req, res);
+      return await fn(req);
     } catch (error) {
       return handleErrors(error);
     }
